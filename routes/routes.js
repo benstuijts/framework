94f18cc53e7f238cc43813fca468192d014549a8
Ben Stuijts Node.js <stuijts@spijkenisse.nl>
2016-07-05 22:04:36 +0000

    setup 25% complete

--- routes/routes.js
+++ routes/routes.js
@@ -5,12 +5,21 @@ const router        = express.Router();
 router.use(function(req, res, next){
     res.locals = {
         author: "Ben Stuijts",
+        copyrights: "Digital markets 2016",
         meta: {
-            name: "framework"
+            name: "framework",
+            facebook: "",
+            twitter: "",
+            google: "",
+            instagram: "",
+            pinterest: "",
         },
         menu: [
             { name: "about", url: ""}, 
-            { name: "contact", url: ""}
+            { name: "contact", url: ""},
+            { name: "main|dropdown"},
+            { name: "main.item1", url: ""},
+            { name: "main.item2", url: ""}
         ]
     };
     next();
@@ -23,8 +32,25 @@ router.get('/', function(req, res) {
         keywords: [],
         css: "framework.css",
         h1: "Framework",
-        h2: "Easy & quick site builder",
+        h2: "Easy & quick site builder, loadingtime < 2s",
     });
+});
+
+router.get('/setup/:code', function(req, res){
+    let code = req.params.code;
+    let setup_code = "bens";
+    
+    if(code === setup_code) {
+        res.render("admin/setup", {
+            title: "Setup",
+            description: "",
+            keywords: [],
+            css: "framework.css",
+            h1: "Framework",
+            h2: "Easy & quick site builder, loadingtime < 2s", 
+        });
+    }
+    
 });
 
 module.exports = router;
