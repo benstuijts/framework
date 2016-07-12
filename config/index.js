45c2a89a92f4ae69468f06962af679f2a91edb3c
Ben Stuijts Node.js <stuijts@spijkenisse.nl>
2016-07-12 21:47:06 +0000

    first test online

---
+++ config/index.js
@@ -0,0 +1,100 @@
+module.exports = {
+    
+        meta: {
+            url: "http://framework-stuijts.c9users.io",
+            name: "framework",
+            author: "Ben Stuijts",
+            copyrights: "Digital markets 2016",
+            facebook: "facebook-account",
+            twitter: "twitter-account",
+            google: "google-account",
+            instagram: "",
+            pinterest: ""
+        },
+        
+        menu: [
+            { name: "about", url: ""}, 
+            { name: "contact", url: ""},
+            { name: "main|dropdown"},
+            { name: "main.item1", url: ""},
+            { name: "main.item2", url: ""}
+        ],
+        
+        header: {
+            image: "https://ucarecdn.com/32620a4e-45d6-46fd-af5d-bcd5cf05b5a7/-/progressive/yes/",
+            callToActionButtonText: "call to action",
+            callToActionButtonHref: ""
+        },
+        
+        contact: {
+            image: "https://ucarecdn.com/4467d9e4-223a-4bae-829b-3f36011f8276/-/progressive/yes/"
+        },
+        
+        whatdowedo: {
+            active: true,
+            title: "What do we do?",
+            missionStatement: "Our Mission Statement",
+            statistics: {
+                active: true,
+                visitorsPerMonth: 124,
+                pages: 10,
+                salesPerMonth: 19,
+                usersOnline: 6
+            }
+        },
+        
+        whatclientssay: {
+            active: true,
+            blockquotes: [
+                { 
+                    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.",
+                    author: "Someone famous",
+                    href: "telefoonaanbiedingen.nl",
+                    anchortext: "facebook pagina",
+                    cite: ""
+                },
+                {
+                    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.",
+                    author: "Someone famous",
+                    href: "",
+                    anchortext: "Source Title",
+                    cite: "AD magazine 7 juli 2016"
+                }
+            ]
+        },
+        
+        story: {
+            active: true,
+            title: "Our story (just for SEO)",
+            introduction: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ullamcorper ac diam sit amet mattis. Cras tincidunt augue eget velit laoreet tincidunt. Donec porttitor pellentesque quam, at tempus nisi. Donec ullamcorper odio libero, eget maximus ipsum consequat eget. Nam feugiat bibendum augue. Donec tristique, lacus at scelerisque interdum, neque risus aliquam ipsum, eu porttitor ligula ex id diam. Aliquam accumsan orci cursus, pulvinar erat vitae, scelerisque est. Pellentesque at porttitor nibh. Suspendisse in dolor vitae erat vulputate molestie sed bibendum dolor. Suspendisse tincidunt euismod est, id mollis erat convallis vitae. Proin viverra ipsum dolor, varius cursus metus porttitor eu. Sed vestibulum sapien ac nibh aliquam, consequat interdum ligula porttitor.",
+            body: "<p>In nec lobortis lectus, ac interdum purus. Nulla facilisi. Aliquam eu dictum nulla. Vestibulum convallis ac est eu vestibulum. In vehicula dui enim, ac vehicula tellus efficitur viverra. Nunc porta felis in elementum venenatis. Duis tortor nibh, ultricies nec lacus non, posuere rhoncus est. Ut consequat vulputate efficitur. Donec ac fermentum neque.</p><p>In tempor elit eget augue tincidunt rutrum. In rhoncus, lorem at fermentum ultricies, est mi consequat mauris, ac elementum risus velit ac ipsum. Cras viverra urna massa, non elementum sapien pharetra id. Phasellus id vestibulum lorem. Mauris ac cursus dui. Vestibulum hendrerit semper nisl in bibendum. Quisque bibendum urna magna, vitae rutrum orci aliquet tempus. Cras pulvinar est quis turpis imperdiet, semper fringilla sem dapibus. Fusce in libero purus. Vivamus ut consectetur tellus. Nulla facilisi. Integer at lorem sapien. Ut quis odio quis sem lobortis pellentesque et sed elit.</p>",
+            outtro: ""
+        },
+        
+        services: {
+            active: true,
+            title: "What makes us special?",
+            subtitle: "Lorem ipsum dolor sit amet consectetur.",
+            items: [
+                { 
+                    title: "E-Commerce",
+                    icon: "shopping-cart",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."
+                },
+                {
+                    title: "Responsive Design",
+                    icon: "laptop",
+                    text: "some brilliant text"
+                },
+                {
+                    title: "Web Security",
+                    icon: "lock",
+                    text: "Wauw! That's nice..."
+                }
+            ]
+        },
+        
+        end: {}
+        
+        
+    };
--- routes/routes.js
+++ routes/routes.js
@@ -2,37 +2,52 @@
 const express       = require('express');
 const router        = express.Router();
 
-router.use(function(req, res, next){
-    res.locals = {
-        author: "Ben Stuijts",
+const data          = { author: "Ben Stuijts ingewikkelde '@string...",
         copyrights: "Digital markets 2016",
         meta: {
+            url: "http://framework-stuijts.c9users.io",
             name: "framework",
-            facebook: "",
-            twitter: "",
-            google: "",
+            facebook: "facebook-account",
+            twitter: "twitter-account",
+            google: "google-account",
             instagram: "",
             pinterest: "",
-        },
-        menu: [
-            { name: "about", url: ""}, 
-            { name: "contact", url: ""},
-            { name: "main|dropdown"},
-            { name: "main.item1", url: ""},
-            { name: "main.item2", url: ""}
-        ]
+        }
     };
+
+const config = require('../config');
+
+console.log(config.author);
+
+//data.replace("'", "komma");
+
+router.use(function(req, res, next){
+    res.locals.add = function(data) {
+        for(var key in data) {
+            this[key] = data[key];
+        }
+    };
+    
+    res.locals.add(config);
+    res.locals.add({data: JSON.stringify(config).replace("'", "&#39;")});
+    
     next();
 });
 
 router.get('/', function(req, res) {
     res.render('home', {
-        title: "",
-        description: "",
-        keywords: [],
+        title: "framework affiliate website",
+        description: "This is a great template for generating a landingpage for an affiliate website. This framework will load very quickly by use of CDN image loading.",
+        keywords: ["template"],
         css: "framework.css",
         h1: "Framework",
         h2: "Easy & quick site builder, loadingtime < 2s",
+        stats: {
+            visitorsPerMonth: Math.round(Math.random()*1000+4),
+            pages: 10,
+            salesPerMonth: Math.round(Math.random()*20+4),
+            usersOnline: Math.round(Math.random()*10+1)
+        }
     });
 });
 
@@ -49,8 +64,21 @@ router.get('/setup/:code', function(req, res){
             h1: "Framework",
             h2: "Easy & quick site builder, loadingtime < 2s", 
         });
+    } else {
+        res.redirect('/');
     }
     
 });
+
+router.get('/adjust/:code', function(req, res) {
+    let code = req.params.code;
+    let setup_code = "bens";
+    if(code === setup_code) {
+    
+        res.send('adjusting existing website');
+        
+    }
+});
+
 
 module.exports = router;
